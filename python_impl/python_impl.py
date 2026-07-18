370c2dbbf7f479b36bd2e09f7acd057a84c788ef
Gabriel Holanda <holanda@Gabriels-MacBook-Air.local>
2026-07-18 16:53:45 -0300

    Implementação Trie Python

---
+++ python_impl/python_impl.py
@@ -0,0 +1,63 @@
+class Trie :
+    def __init__(self):
+        self.root = Node()
+    
+    def add(self,word):
+        current = self.root
+        current.occurrence += 1
+
+        for c in word:
+            if c not in current.children:
+                current.children[c] = Node()
+            
+            current = current.children[c]
+            current.occurrence += 1
+
+        current.isEnd = True
+
+    def countPrefix(self,word):
+        current = self.root
+
+        for c in word:
+            if c not in current.children:
+                return 0
+
+            current = current.children[c]
+        
+        return current.occurrence
+
+    def checkWord(self,word):
+        current = self.root
+
+        for c in word:
+            if c not in current.children:
+                return False
+
+            current = current.children[c]
+
+        return current.isEnd
+
+    def remove(self,word):
+        if not self.checkWord(word):
+            return
+
+        current = self.root
+        current.occurrence -= 1
+        for c in word:
+            next = current.children[c]
+            next.occurrence -= 1
+
+            if next.occurrence == 0:
+                del current.children[c]
+                return
+
+            current = next
+        
+        current.isEnd = False
+
+
+class Node :
+    def __init__(self):
+        self.children = {}
+        self.occurrence = 0
+        self.isEnd = False
