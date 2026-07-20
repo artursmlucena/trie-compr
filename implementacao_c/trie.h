b922aaa8af4b2a7d55ffba5134dace04261ac694
Joao_Victor <jbezerra15@hotmail.com>
2026-07-20 11:36:34 -0300

    Atualizados nomes de funcoes em arquivo header

--- implementacao_c/trie.h
+++ implementacao_c/trie.h
@@ -6,12 +6,11 @@ typedef struct trie trie;
 trie* trie_new();
 
 void trie_add(trie* t, const char* word);
-bool trie_check(const trie* t, const char* word);
+bool trie_check_word(const trie* t, const char* word);
 bool trie_remove(trie* t, const char* word);
 
 void trie_free(trie* t);
 
-int trie_cnt(const trie* t, const char* word);
-int trie_get_words(const trie* t);
+int trie_count_prefix(const trie* t, const char* word);
 
 #endif  // !TRIE_H
