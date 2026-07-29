72560318fe3339aa51afa0f19094432bbaaad6f2
artursmlucena <artur.santana.medeiros.de.lucena@ccc.ufcg.edu.br>
2026-07-29 10:44:52 -0300

    trocando a minha trie pela trie padrão

--- implementacao_java/Main.java
+++ implementacao_java/Main.java
@@ -3,7 +3,13 @@ class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    Trie trie = new Trie();
+    StandartTrie trie = new StandartTrie();
+
+    int n = Integer.parseInt(sc.nextLine());
+    for(int i = 0; i < n; i++) {
+      trie.add(sc.nextLine());
+    }
+    
   }
 
 }
--- python_impl/python_impl.py
+++ python_impl/python_impl.py
@@ -1,21 +1,21 @@
-class Trie :
+class Trie:
     def __init__(self):
         self.root = Node()
-    
-    def add(self,word):
+
+    def add(self, word):
         current = self.root
         current.occurrence += 1
 
         for c in word:
             if c not in current.children:
                 current.children[c] = Node()
-            
+
             current = current.children[c]
             current.occurrence += 1
 
         current.isEnd += 1
 
-    def countPrefix(self,word):
+    def countPrefix(self, word):
         current = self.root
 
         for c in word:
@@ -23,10 +23,10 @@ class Trie :
                 return 0
 
             current = current.children[c]
-        
+
         return current.occurrence
 
-    def checkWord(self,word):
+    def checkWord(self, word):
         current = self.root
 
         for c in word:
@@ -37,7 +37,7 @@ class Trie :
 
         return current.isEnd > 0
 
-    def remove(self,word):
+    def remove(self, word):
         if not self.checkWord(word):
             return
 
@@ -52,11 +52,11 @@ class Trie :
                 return
 
             current = next
-        
+
         current.isEnd -= 1
 
 
-class Node :
+class Node:
     def __init__(self):
         self.children = {}
         self.occurrence = 0
