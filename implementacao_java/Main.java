2dee78e6e36e994c95d8918aa292e808b489365b
artursmlucena <artur.santana.medeiros.de.lucena@ccc.ufcg.edu.br>
2026-07-31 16:01:46 -0300

    Atualização da Main de Java

--- implementacao_java/Main.java
+++ implementacao_java/Main.java
@@ -7,7 +7,23 @@ class Main {
     StandartTrie trie = new StandartTrie();
     int n = Integer.parseInt(br.readLine());
     for(int i = 0; i < n; i++) {
-     trie.add(br.readLine()); 
+      String[] in = br.readLine().split(" ");
+      int query = Integer.parseInt(in[0]);
+      String palavra = in[1];
+      
+
+      if (query == 1) {
+        trie.add(palavra);
+
+      }else if (query == 2) {
+        trie.checkWord(palavra);
+
+      }else if (query == 3) {
+        trie.countPrefix(palavra);
+
+      }else if (query == 4) {
+        trie.remove(palavra);
+      }
     }
   }
 }
