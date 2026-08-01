6f656ab6003c487a4edf3935e09ac1121a1c5040
Joao_Victor <jbezerra15@hotmail.com>
2026-07-31 21:31:41 -0300

    Atualizada main para suportar multiplas operacoes

--- implementacao_c/main.c
+++ implementacao_c/main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 
 #include "trie.h"
+char buffer[2000007];
 
 int main() {
-  int n;
-  scanf("%d", &n);
-  char buffer[2000007];
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n;
+    scanf("%d", &n);
+    trie* Trie = trie_new();
+    for (int i = 0; i < n; i++) {
+      int operation;
+      scanf("%d %s", &operation, buffer);
 
-  trie* Trie = trie_new();
+      if (operation == 1) trie_add(Trie, buffer);
 
-  for (int i = 0; i < n; i++) {
-    scanf("%s", buffer);
-    trie_add(Trie, buffer);
+      if (operation == 2) {
+        bool k = trie_check_word(Trie, buffer);
+        if (k)
+          printf("YES\n");
+        else
+          printf("NO\n");
+      }
+
+      if (operation == 3) {
+        int cnt = trie_count_prefix(Trie, buffer);
+        printf("%d\n", cnt);
+      }
+
+      if (operation == 4) trie_remove(Trie, buffer);
+    }
+
+    trie_free(Trie);
   }
 }
