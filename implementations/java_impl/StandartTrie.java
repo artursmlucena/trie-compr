277e36bc72c5ab19aa6699d8b2c44052a6ab81bb
artursmlucena <artur.santana.medeiros.de.lucena@ccc.ufcg.edu.br>
2026-08-07 11:14:55 -0300

    fix standart trie

--- implementations/java_impl/StandartTrie.java
+++ implementations/java_impl/StandartTrie.java
@@ -2,66 +2,74 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Node {
-    Map<Character, Node> children = new HashMap<>();
-    int occurrence = 0;
-    int isEnd = 0;
+  Node[] children = new Node[26];
+  int occurrence = 0;
+  int isEnd = 0;
 }
 
 public class StandartTrie {
-    private Node root;
+  private Node root;
 
-    public StandartTrie() {
-        root = new Node();
+  public StandartTrie() {
+      root = new Node();
+  }
+
+  public void add(String word) {
+    Node current = root;
+    current.occurrence++;
+    for (char c : word.toCharArray()) {
+
+      if(current.children[c - 'a'] == null) {
+        current.children[c - 'a'] = new Node();
+      }
+
+      current = current.children[c - 'a'];
+      current.occurrence++;
     }
+    current.isEnd++;
+  }
 
-    public void add(String word) {
-        Node current = root;
-        current.occurrence++;
-        for (char c : word.toCharArray()) {
-            current.children.putIfAbsent(c, new Node());
-            current = current.children.get(c);
-            current.occurrence++;
+  public int countPrefix(String word) {
+    Node current = root;
+    for (char c : word.toCharArray()) {
+        if (current.children[c - 'a'] == null) {
+            return 0;
         }
-        current.isEnd++;
+        current = current.children[c - 'a'];
     }
+    return current.occurrence;
+  }
 
-    public int countPrefix(String word) {
-        Node current = root;
-        for (char c : word.toCharArray()) {
-            if (!current.children.containsKey(c)) {
-                return 0;
-            }
-            current = current.children.get(c);
+  public boolean checkWord(String word) {
+    Node current = root;
+    for (char c : word.toCharArray()) {
+
+        if (current.children[c - 'a'] == null) {
+            return false;
         }
-        return current.occurrence;
+
+        current = current.children[c - 'a'];
     }
 
-    public boolean checkWord(String word) {
-        Node current = root;
-        for (char c : word.toCharArray()) {
-            if (!current.children.containsKey(c)) {
-                return false;
-            }
-            current = current.children.get(c);
-        }
-        return current.isEnd > 0;
+    return current.isEnd > 0;
+  }
+
+  public void remove(String word) {
+    if (!checkWord(word)) {
+        return;
     }
 
-    public void remove(String word) {
-        if (!checkWord(word)) {
+    Node current = root;
+    current.occurrence--;
+    for (char c : word.toCharArray()) {
+        Node next = current.children[c - 'a'];
+        next.occurrence--;
+        if (next.occurrence == 0) {
+            current.children[c - 'a'] = null;
             return;
         }
-        Node current = root;
-        current.occurrence--;
-        for (char c : word.toCharArray()) {
-            Node next = current.children.get(c);
-            next.occurrence--;
-            if (next.occurrence == 0) {
-                current.children.remove(c);
-                return;
-            }
-            current = next;
-        }
-        current.isEnd--;
+        current = next;
     }
+    current.isEnd--;
+  }
 }
