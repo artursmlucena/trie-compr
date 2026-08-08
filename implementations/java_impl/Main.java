43520be88b65f3d1a8d3f34419514af01b60cecf
artursmlucena <artur.santana.medeiros.de.lucena@ccc.ufcg.edu.br>
2026-08-07 21:30:55 -0300

    add warmup java

--- implementations/java_impl/Main.java
+++ implementations/java_impl/Main.java
@@ -6,13 +6,51 @@ class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    PrintWriter out2 = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
 
     int t = Integer.parseInt(br.readLine());
+    
+    long start = System.nanoTime();
+    warmup(br, out2);
+    long end = System.nanoTime();
+    out2.println(end - start);
+    out2.flush();
+
 
     while (t-- != 0)
       test(br, out);
       
     out.flush();
+  }
+
+  public static void warmup(BufferedReader br, PrintWriter out) throws IOException {
+    int n = Integer.parseInt(br.readLine());
+    StandardTrie trie = new StandardTrie();
+
+    for (int i = 0; i < n; i++) {
+      String[] in = br.readLine().split(" ");
+      int query = Integer.parseInt(in[0]);
+      String palavra = in[1];
+
+      if (query == 1) {
+        trie.add(palavra);
+
+      } else if (query == 2) {
+        if (trie.checkWord(palavra))
+          out.println("YES");
+        else
+          out.println("NO");
+
+      } else if (query == 3) {
+        int count = trie.countPrefix(palavra);
+        out.println(count);
+
+      } else if (query == 4) {
+        trie.remove(palavra);
+      }
+    }
+    out.flush();
+
   }
 
   public static void test(BufferedReader br, PrintWriter out) throws IOException {
